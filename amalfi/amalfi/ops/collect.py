e1877b78315d3ee943e676a8082fccb9110dcafd
Carlo Casorzo <carlo@deepblu.dev>
2024-10-12 17:21:46 +0200

    docs: improve docstrings for filter and collect

--- amalfi/amalfi/ops/collect.py
+++ amalfi/amalfi/ops/collect.py
@@ -7,6 +7,26 @@ def collect_[I, O](fn: Fn[I, Iterator[O]]) -> Fn[I, list[O]]:
     """
     Collect all items from an iterator into a list.
     Useful for turning a generator into a list inside a pipeline.
+
+    Designed as a curried function for use in pipelines.
+
+    Args:
+        fn (Fn[I, Iterator[O]]): A function that yields an iterator of items to collect
+
+    Returns:
+        Fn[I, list[O]]: The curried collector that collects the items into a list
+
+    Raises:
+        Any exception raised by the function `fn` will be propagated.
+
+    Examples:
+        >>> double = lambda x: (x * 2 for x in x)
+        >>> collect_double = collect_(double)
+        >>> collect_double([1, 2, 3])
+        [2, 4, 6]
+
+        >>> Pipeline.pipe([1, 2, 3]) | collect_(double) | sum
+        12
     """
 
     def inner(input: I) -> list[O]:
@@ -19,6 +39,27 @@ def acollect[I, O](fn: Fn[I, AsyncIterator[O]]) -> AsyncFn[I, list[O]]:
     """
     Collect all items from an asynchronous iterator into a list.
     Useful for turning a generator into a list inside an async pipeline.
+
+    Designed as a curried function for use in pipelines.
+
+    Args:
+        fn (Fn[I, AsyncIterator[O]]): A function that yields an asynchronous
+        iterator of items to collect
+
+    Returns:
+        AsyncFn[I, list[O]]: The curried collector that collects the items into a list
+
+    Raises:
+        Any exception raised by the function `fn` will be propagated.
+
+    Examples:
+        >>> async def double(x):
+        ...     for i in x:
+        ...         await asyncio.sleep(0.01)
+        ...         yield i * 2
+        >>> acollect_double = acollect_(double)
+        >>> await acollect_double([1, 2, 3])
+        [2, 4, 6]
     """
 
     async def inner(input: I) -> list[O]:
--- amalfi/amalfi/ops/filter.py
+++ amalfi/amalfi/ops/filter.py
@@ -20,7 +20,25 @@ def filter_[T, S](fn: Fn[T, bool] | TypeGuardFn[T, S] | None) -> IterFn[T, S]:
     """
     Filter elements of an iterable using a predicate function.
 
+    Designed as a curried function for use in pipelines.
     This function supports TypeGuard predicates for type narrowing.
+
+    Args:
+        fn (Fn[T, bool] | TypeGuardFn[T, S] | None): A predicate function
+
+    Returns:
+        IterFn[T, S]: The curried filter that yields the filtered values
+
+    Raises:
+        Any exception raised by the predicate function will be propagated.
+
+    Examples:
+        >>> is_even = lambda x: x % 2 == 0
+        >>> filter_even = filter_(is_even)
+        >>> list(filter_even([1, 2, 3, 4]))
+        [2, 4]
+        >>> Pipeline.pipe([1, 2, 3, 4]) | filter_(is_even) | list
+        [2, 4]
     """
 
     def inner(iterable: Iterable[T]) -> Iterable[S]:
@@ -44,7 +62,32 @@ def afilter[T, S](
     Filter elements of an iterable using an async predicate function.
     It will concurrently evaluate the predicate function for each element.
 
+    Designed as a curried function for use in pipelines.
     This function supports async TypeGuard predicates for type narrowing.
+
+    Args:
+        fn (AsyncFn[T, bool] | AsyncTypeGuardFn[T, S]): An async predicate function
+
+    Returns:
+        AsyncIterFn[T, S]: The curried filter that yields the filtered values
+
+    Raises:
+        Any exception raised by the predicate function will be propagated.
+
+    Examples:
+        >>> async def wait_and_check_even(x: int) -> bool:
+        ...     await asyncio.sleep(0.01)
+        ...     return x % 2 == 0
+        >>> afilter_even = afilter_(wait_and_check_even)
+        >>> list(await afilter_even([1, 2, 3, 4]))
+        [2, 4]
+        >>> result = await (
+                Pipeline.apipe([1, 2, 3, 4])
+                | afilter_(wait_and_check_even)
+                | list
+            ).run()
+        >>> result
+        [2, 4]
     """
 
     async def inner(iterable: Iterable[T]) -> Iterable[S]:
